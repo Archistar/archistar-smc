856161ccee330d06286d3a127ed91583ccec1ff5
unknown <efrantar@student.tgm.ac.at>
2014-07-18 09:18:18 +0200

    added documentation

--- src/main/java/at/archistar/crypto/math/CustomMatrix.java
+++ src/main/java/at/archistar/crypto/math/CustomMatrix.java
@@ -4,23 +4,43 @@ import de.flexiprovider.common.math.codingtheory.GF2mField;
 import de.flexiprovider.common.math.linearalgebra.GF2mMatrix;
 
 /**
+ * <p>A matrix operating in GF(256).</p>
+ * 
+ * <p>Uses {@link GF256} for optimized operations and provides some additional methods in contrary to the flexiprovider-
+ * class.</p>
  *
- * GF2mMatrix does not support rightMultiply, add a (very) simple implementation
- * of this operation.
- *
+ * @author Elias Frantar <i>(added documentation)</i>
  * @author Andreas Happe <andreashappe@snikt.net>
+ * @version 2014-7-18
  */
 public class CustomMatrix extends GF2mMatrix {
 	private static final GF2mField gf256 = new GF2mField(8, 0x11d); // Galois-Field (x^8 + x^4 + x^3 + x + 1 = 0) / 285
 	
+	/**
+     * Constructor
+     * @param data the data to put into the matrix
+     */
     public CustomMatrix(int[][] data) {
         super(gf256, data);
     }
 
+    /**
+	 * Constructor
+	 * @param encoded the encoded matrix (got via {@link #getEncoded()})
+	 */
     public CustomMatrix(byte[] encoded) {
         super(gf256, encoded);
     }
 
+    /**
+     * Performs a matrix * vector multiplication.
+     * 
+     * <b>NOTE:</b> Matrix multiplication is not commutative. (A*B != B*A) and so does only work if A(MxN) and B(NxO).
+     * 		 		Throws an {@link ArithmeticException} if this condition is violated.
+     * 
+     * @param vec the vector to multiply the matrix with (a 1D-matrix)
+     * @return the product of the matrix and the given vector <i>(matrix * vector)</i>
+     */
     public int[] rightMultiply(int vec[]) {
 
         assert (vec.length == matrix.length);
@@ -38,6 +58,11 @@ public class CustomMatrix extends GF2mMatrix {
         return result;
     }
 
+    /**
+     * Returns the <i>i<sup>th</sup></i> row of the matrix.
+     * @param i index of the row to return
+     * @return the <i>i<sup>th</sup></i> row of the matrix (starting at 0)
+     */
     public int[] getRow(int i) {
         return matrix[i];
     }
