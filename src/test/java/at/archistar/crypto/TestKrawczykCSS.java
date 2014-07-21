a71cb94c42ee43ce6cbcba98ffe690f7185ee480
Elias Frantar <efrantar@student.tgm.ac.at>
2014-07-21 12:48:33 +0200

    added new test-cases

--- src/test/java/at/archistar/crypto/TestKrawczykCSS.java
+++ src/test/java/at/archistar/crypto/TestKrawczykCSS.java
@@ -1,7 +1,11 @@
 package at.archistar.crypto;
 
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import at.archistar.crypto.KrawczykCSS;
@@ -12,19 +16,51 @@ import at.archistar.crypto.random.FakeRandomSource;
 import static org.fest.assertions.api.Assertions.*;
 
 /**
- * - * @author Andreas Happe <andreashappe@snikt.net>
+ * Test for {@link KrawczykCSS}
+ * 
+ * @author Elias Frantar <i>(added additional test-cases)</i>
+ * @author Andreas Happe <andreashappe@snikt.net>
+ * @version 2014-7-21
  */
 public class TestKrawczykCSS {
+	byte data[] = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+	private SecretSharing algorithm;
+	
+	/* setup and tear-down */
+	@Before
+	public void setup() {
+		algorithm = new KrawczykCSS(8, 5, new FakeRandomSource());
+	}
+	@After
+	public void tearDown() {
+		algorithm = null;
+	}
 
+	/* tests */
+	
+	/* should succeed reconstructing */
     @Test
     public void simpleRoundTest() throws WeakSecurityException, GeneralSecurityException {
-        byte data[] = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-
-        SecretSharing algorithm = new KrawczykCSS(8, 5, new FakeRandomSource());
-
         Share shares[] = algorithm.share(data);
         assertThat(shares.length).isEqualTo(8);
 
+        byte reconstructedData[] = algorithm.reconstruct(shares);
+        assertThat(reconstructedData).isEqualTo(data);
+    }
+    
+    @Test
+    public void notAllSharesTest() throws WeakSecurityException, GeneralSecurityException {
+        Share shares[] = algorithm.share(data);
+        Share shares1[] = Arrays.copyOfRange(shares, 0, 6);
+
+        byte reconstructedData[] = algorithm.reconstruct(shares1);
+        assertThat(reconstructedData).isEqualTo(data);
+    }
+    @Test
+    public void shuffledSharesTest() throws WeakSecurityException, GeneralSecurityException {
+        Share shares[] = algorithm.share(data);
+        Collections.shuffle(Arrays.asList(shares));
+        
         byte reconstructedData[] = algorithm.reconstruct(shares);
         assertThat(reconstructedData).isEqualTo(data);
     }
--- src/test/java/at/archistar/crypto/TestShamirPSS.java
+++ src/test/java/at/archistar/crypto/TestShamirPSS.java
@@ -3,7 +3,11 @@ package at.archistar.crypto;
 import static org.fest.assertions.api.Assertions.*;
 
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import at.archistar.crypto.SecretSharing;
@@ -13,17 +17,49 @@ import at.archistar.crypto.data.Share;
 import at.archistar.crypto.random.FakeRandomSource;
 
 /**
- * - * @author Andreas Happe <andreashappe@snikt.net>
+ * Tests for {@link ShamirPSS}.
+ * 
+ * @author Elias Frantar <i>(added additional test-cases)</i>
+ * @author Andreas Happe <andreashappe@snikt.net>
+ * @version 2014-7-21
  */
 public class TestShamirPSS {
-
+    byte data[] = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+	private SecretSharing algorithm;
+	
+	/* setup and tear-down */
+	@Before
+	public void setup() {
+		algorithm = new ShamirPSS(8, 3, new FakeRandomSource());
+	}
+	@After
+	public void tearDown() {
+		algorithm = null;
+	}
+	
+	/* tests */
+	
+	/* should succeed reconstructing */
     @Test
     public void simpleRoundTest() throws WeakSecurityException, GeneralSecurityException {
-        byte data[] = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-
-        SecretSharing algorithm = new ShamirPSS(4, 3, new FakeRandomSource());
-
         Share shares[] = algorithm.share(data);
+
+        byte reconstructedData[] = algorithm.reconstruct(shares);
+        assertThat(reconstructedData).isEqualTo(data);
+    }
+    
+    @Test
+    public void notAllSharesTest() throws WeakSecurityException, GeneralSecurityException {
+        Share shares[] = algorithm.share(data);
+        Share[] shares1 = Arrays.copyOfRange(shares, 1, 4);
+
+        byte reconstructedData[] = algorithm.reconstruct(shares1);
+        assertThat(reconstructedData).isEqualTo(data);
+    }
+    @Test
+    public void shuffledSharesTest() throws WeakSecurityException, GeneralSecurityException {
+        Share shares[] = algorithm.share(data);
+        Collections.shuffle(Arrays.asList(shares));
 
         byte reconstructedData[] = algorithm.reconstruct(shares);
         assertThat(reconstructedData).isEqualTo(data);
