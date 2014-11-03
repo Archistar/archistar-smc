8d54744b585d75d2209762753547e339cfbbf1ff
Andreas Happe <andreashappe@snikt.net>
2014-11-03 14:05:19 +0100

    fix whitespace errors

--- src/main/java/at/archistar/crypto/math/NTTTextbook.java
+++ src/main/java/at/archistar/crypto/math/NTTTextbook.java
@@ -15,7 +15,7 @@ public class NTTTextbook extends AbstractNTT {
         
         if (n == 1) {
             return a;
-        } else if ( n%2 == 0) {
+        } else if (n%2 == 0) {
             return new NTTSlow(gf).ntt(a, w);
         } else {
                         
@@ -24,13 +24,13 @@ public class NTTTextbook extends AbstractNTT {
             int[] combined = new int[n];
             
             /* create even */
-            for(int i = 0; i < n; i+=2) {
+            for (int i = 0; i < n; i += 2) {
                 even[i/2] = a[i];
             }
-            even = ntt(even, gf.pow(w,2 ));
+            even = ntt(even, gf.pow(w, 2));
 
             /* create odd */
-            for(int i = 1; i < n; i+=2) {
+            for (int i = 1; i < n; i += 2) {
                 odd[i/2] = a[i];
             }
             odd = ntt(odd, gf.pow(w, 2));
