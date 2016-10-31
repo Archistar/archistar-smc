54578714cfd9015e4b9b99c0df699745e6a13278
Florian Wohner <florian.wohner@ait.ac.at>
2016-10-31 18:32:34 +0100

    fix test to correctly calculate size of shares (and add another test) once again that thing with the block ciphers; added a test using a ChaCha20 stream cipher

--- src/test/java/at/archistar/crypto/secretsharing/TestKrawczykCSS.java
+++ src/test/java/at/archistar/crypto/secretsharing/TestKrawczykCSS.java
@@ -8,6 +8,7 @@ import at.archistar.crypto.math.GFFactory;
 import at.archistar.crypto.math.gf256.GF256Factory;
 import at.archistar.crypto.random.FakeRandomSource;
 import at.archistar.crypto.symmetric.AESEncryptor;
+import at.archistar.crypto.symmetric.ChaCha20Encryptor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -36,7 +37,23 @@ public class TestKrawczykCSS extends BasicSecretSharingTest {
     }
 
     @Test
-    public void it_produces_shares_of_the_right_size() throws IOException {
+    public void it_produces_shares_of_the_right_size_block_cipher() throws IOException {
+        final Share[] shares = algorithm.share(data);
+        final int new_data_length = ((data.length / 16) + 1) * 16;
+        final int new_length = new_data_length % k == 0 ? new_data_length / k : (new_data_length / k) + 1;
+        for (Share s : shares) {
+            assertThat(s.getYValues().length).isEqualTo(new_length);
+        }
+    }
+
+    @Test
+    public void it_produces_shares_of_the_right_size_stream_cipher() throws IOException, WeakSecurityException {
+        GFFactory gfFactory = new GF256Factory();
+        DecoderFactory df = new ErasureDecoderFactory(gfFactory);
+        GF gf = gfFactory.createHelper();
+
+        algorithm = new KrawczykCSS(n, k, new FakeRandomSource(), new ChaCha20Encryptor(), df, gf);
+
         final Share[] shares = algorithm.share(data);
         final int new_length = data.length % k == 0 ? data.length / k : (data.length / k) + 1;
         for (Share s : shares) {
