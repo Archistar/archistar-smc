fee5e44a030219bee9dab20c2da7666a3e9f1ef0
Florian Wohner <florian.wohner@ait.ac.at>
2017-06-07 16:23:11 +0200

    restore partial reconstruction test for CSS engine

--- src/test/java/at/archistar/crypto/TestCSSEngine.java
+++ src/test/java/at/archistar/crypto/TestCSSEngine.java
@@ -2,33 +2,33 @@ package at.archistar.crypto;
 
 import at.archistar.crypto.data.CSSShare;
 import at.archistar.crypto.data.InvalidParametersException;
-import at.archistar.crypto.informationchecking.RabinBenOrRSS;
 import at.archistar.crypto.data.Share;
-import at.archistar.crypto.secretsharing.ReconstructionException;
-import at.archistar.crypto.secretsharing.WeakSecurityException;
+import at.archistar.crypto.informationchecking.RabinBenOrRSS;
 import at.archistar.crypto.random.FakeRandomSource;
 import at.archistar.crypto.random.RandomSource;
+import at.archistar.crypto.secretsharing.ReconstructionException;
+import at.archistar.crypto.secretsharing.WeakSecurityException;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.fest.assertions.api.Assertions.*;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
 
 /**
  * Tests for {@link RabinBenOrRSS}
  */
 public class TestCSSEngine {
 
+    private final static RandomSource rng = new FakeRandomSource();
     private final byte data[] = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
     private final int n = 8;
     private final int k = 5;
-    private final static RandomSource rng = new FakeRandomSource();
-    private CryptoEngine algorithm;
+    private CSSEngine algorithm;
 
     /**
      * create a new RabinBenOr CryptoEngine
@@ -115,6 +115,35 @@ public class TestCSSEngine {
         final int new_length = data.length % k == 0 ? data.length / k : (data.length / k) + 1;
         for (Share s : shares) {
             assertThat(s.getYValues().length).isEqualTo(new_length);
+        }
+    }
+
+    /**
+     * test the reconstruction of partial shares by chopping off all possible parts
+     * of the shared data; therefore, reconstruction happens in increments of k
+     * (the index computations required for byte-precise access are the responsibility
+     * of the client)
+     */
+    @Test
+    public void reconstructPartialShares() throws InvalidParametersException, ReconstructionException {
+        CSSShare[] shares = algorithm.share(data);
+        assertThat(shares.length).isEqualTo(n);
+        CSSShare[] truncated = new CSSShare[n];
+        int sharedLength = shares[0].getYValues().length;
+        for (int i = 0; i < sharedLength; i++) {
+            for (int j = i + 1; j <= sharedLength; j++) {
+                for (int s = 0; s < n; s++) {
+                    CSSShare share = shares[s];
+                    truncated[s] = new CSSShare((byte) share.getX(), Arrays.copyOfRange(share.getYValues(), i, j),
+                            share.getFingerprints(), share.getOriginalLength(), 1, share.getKey());
+                }
+                byte[] reconstructed = algorithm.reconstructPartial(truncated, i * k);
+                int trunc_begin = i * k;
+                int trunc_end = Math.min(data.length, j * k);
+                int truncation = j * k > data.length ? data.length - (i * k) : (j - i) * k;
+                // truncation of the reconstructed data is actually only needed when we are on the last block
+                assertThat(Arrays.copyOf(reconstructed, truncation)).isEqualTo(Arrays.copyOfRange(data, trunc_begin, trunc_end));
+            }
         }
     }
 }
